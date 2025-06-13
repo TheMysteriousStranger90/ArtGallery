a190de5f371109f2b63e326115811e26d31909d1
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2025-06-13 06:29:58 +0300

    added UserEdit...

---
+++ ArtGallery.ClientApp/ViewModels/UserEditModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtGallery.ClientApp.ViewModels;
+
+public class UserEditModel
+{
+    [Required(ErrorMessage = "First name is required")]
+    [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required")]
+    [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Username is required")]
+    [StringLength(30, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 30 characters")]
+    public string UserName { get; set; }
+}
